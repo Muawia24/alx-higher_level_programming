25ee80e30b1101eac77d17c711ffae347f96eefb
Muawia24 <amuawia666@gmail.com>
2024-03-14 16:05:07 -0700

    100-my_calculator.py

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import sys
+
+args = len(sys.argv) - 1
+
+if args != 3:
+    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    sys.exit(1)
+a = int(sys.argv[1])
+operator = "+-*/"
+op = sys.argv[2]
+b = int(sys.argv[3])
+
+if op not in operator:
+    print("Unknown operator. Available operators: +, -, * and /")
+    sys.exit(1)
+
+print("{} {} {} = {}".format(a, op, b, a + b))
+sys.exit(0)
