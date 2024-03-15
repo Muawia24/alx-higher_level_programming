177808677e0de3c552bc773d07db67b6275437dc
Muawia24 <amuawia666@gmail.com>
2024-03-14 18:43:11 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     elif args == 1:
         print("{} argument:".format(args))
     elif args > 1:
-        print("{} argument:".format(args))
+        print("{} arguments:".format(args))
 
     for i in range(1, args + 1):
         print("{}: {}".format(i, sys.argv[i]))
