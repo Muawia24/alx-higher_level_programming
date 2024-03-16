c5060ee33c95f446e5c94b86c644796282d69d43
Muawia24 <amuawia666@gmail.com>
2024-03-16 10:05:11 -0700

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 def no_c(my_string):
 
-    return "".join([char for char in my_string if char not in "Cc"])
+    return ("".join([char for char in my_string if char not in ['c', 'C']]))
