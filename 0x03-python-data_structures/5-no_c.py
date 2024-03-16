2d2fa48079ef9bef3b2cc018a84faedbf29c20bf
Muawia24 <amuawia666@gmail.com>
2024-03-16 09:46:32 -0700

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 def no_c(my_string):
 
-    new_str = ''
+    new_str = ""
     for char in my_string:
         if char not in "Cc":
-            new_str = new_str + char
-    my_string = new_str
+            new_str += char
     return new_str
