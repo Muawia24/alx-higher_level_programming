9d60c88a6961bb2a1ff70f30a6f380ba5002cd4a
Muawia24 <amuawia666@gmail.com>
2024-03-18 17:34:51 -0700

    100-print_python_list_info.c

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -9,15 +9,15 @@ void print_python_list_info(PyObject *p)
 {
 	int i = 0;
 
-	list_size = py_size(p);
-	allocate = ((PyListobject *)p)->allocate
+	list_size = Py_SIZE(p);
+	allocate = ((PyListobject *)p)->allocated
 
 	printf("[*] Size of the Python List = %d\n", size);
 	printf("[*] Allocated = %d\n", allocate);
 	while (i < list_size)
 	{
 
-		printf("Element %d: %s\n", i, py_type(PyList_GetItem(p, i))->tp_name);
+		printf("Element %d: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
 		i++;
 	}
 }
