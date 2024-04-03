a78117bd8f18eb0a5c833d1260e6297637f3dc1d
Muawia24 <amuawia666@gmail.com>
2024-04-03 07:29:59 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+'''
+    Square class definition
+'''
+
+
+class Square:
+    '''
+    properties of Square
+
+    Attributes:
+        size: size of square
+    '''
+
+    '''
+        creates new instance of square
+
+        Args:
+            size: size of square
+
+        '''
+    def __init__(self, size):
+        self.__size = size
