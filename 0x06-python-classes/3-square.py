c8300898007ee9c2e411936dad0d54c6fb03ac90
Muawia24 <amuawia666@gmail.com>
2024-04-03 11:14:48 -0700

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+'''Square class definition'''
+
+
+class Square:
+    '''     properties of square
+
+            attributes:
+                size: size of square
+    '''
+
+    def __init__(self, size):
+        '''
+        instansiate an instance of square
+
+        Args:
+            size: size of square
+        '''
+        if type(size) == int:
+            self.__size = size
+
+        else:
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        '''
+         public method to calc area of square
+
+          Return: the area of a square
+        '''
+        return self.__size * self.__size
