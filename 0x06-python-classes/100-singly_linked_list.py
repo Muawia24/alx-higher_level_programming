d2972e72079f7cccb139413dc47c55e331d3385c
Muawia24 <amuawia666@gmail.com>
2024-04-04 18:40:17 -0700

    6-square.py

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+
+class Node:
+
+    def __init__(self, data, next_node=None):
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        if type(value) == int:
+            self.__data = value
+        else:
+            raise TypeError("data must be an integer")
+
+    @property
+    def next_node(self):
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        if value != None and type(value) != Node:
+            raise TypeError("next_node must be a Node object")
+
+        self.__next_node = value
+
+class SinglyLinkedList:
+
+    def __init__(self):
+        self.__head = None
+
+    def __str__(self):
+        temp = self.__head
+        h_list = []
+
+        while temp:
+            h_list.sort()
+            h_list.append(str(temp.data))
+            temp = temp.next_node
+        h_list.sort(key=int)
+        return ("\n".join(h_list))
+
+    def sorted_insert(self, value):
+        if self.__head == None:
+            new_node = Node(value)
+            new_node.next_node = self.__head
+            self.__head = new_node
+
+        else:
+            new_node = Node(value)
+            new_node.data = value
+            new_node.node_next = self.__head
+            self.__head = new_node
+
--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -18,8 +18,8 @@ class Square:
             size: size of square
             position: cordinates
         '''
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
@@ -69,7 +69,7 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         if type(vlaue[0]) != int or type(value[1]) != int:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if value[1] < 0 or value[1] < 0:
+        if value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
