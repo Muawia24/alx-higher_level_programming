ae667757a730d394363f214beb0c0692b72099a5
Muawia24 <amuawia666@gmail.com>
2024-04-15 14:02:26 -0700

    11-square.py

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Square module """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Square methods """
+    def __init__(self, size):
+        """ Instantiation method """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """ returns Square area """
+        return self.__size ** 2
+
+    def __str__(self):
+        """ returns Square string representation """
+        return "[Square] {}/{}".format(self.__size, self.__size)
