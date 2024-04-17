06fcdac93297dcf2c683c8c506f9b729eeba6eef
Muawia24 <amuawia666@gmail.com>
2024-04-17 16:42:15 -0700

    8-class_to_json.py

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" class to json module """
+import json
+
+
+def class_to_json(obj):
+    """
+     returns the dictionary description with simple data
+     structure (list, dictionary,
+     string, integer and boolean) for JSON
+    """
+    d = {}
+    for key, value in obj.__dict__.items():
+        if type(value) in [list, dict, str, bool, int]:
+            d[key] = value
+
+    return d
