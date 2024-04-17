517b555eb14c399dad3b1b8d0dd6a25797266421
Muawia24 <amuawia666@gmail.com>
2024-04-16 17:33:33 -0700

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,5 +8,5 @@ def read_file(filename=""):
     """
 
     with open(filename, "r", encoding="utf-8") as f:
-        fd = f.read()
-        print(fd)
+        for fd in f:
+            print(fd, end='')
