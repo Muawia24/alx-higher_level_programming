455a01a5424a44dd56460b23b4bcd3722793f91b
Muawia24 <amuawia666@gmail.com>
2024-04-17 18:25:26 -0700

    11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" Student module """
+
+
+class Student:
+    """
+    that defines a student by,
+    it first, last name and age
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        retrieves a dictionary representation of a
+        Student instance
+        """
+        if attrs is not None:
+            keys = [name for name in self.__dict__ if name in attrs]
+        else:
+            keys = [name for name in self.__dict__]
+
+        d = {}
+        for key in keys:
+            value = getattr(self, key)
+            if type(value) in [list, dict, str, bool, int]:
+                d[key] = value
+
+        return d
+
+    def reload_from_json(self, json):
+        """
+        replaces all attributes of the Student instance
+        """
+        for key in json:
+            setattr(self, key, json[key])
