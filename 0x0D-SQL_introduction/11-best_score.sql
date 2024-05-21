72934bf4f1c870610005dcc9249a6bb073cc3e95
Muawia24 <amuawia666@gmail.com>
2024-05-21 16:45:08 -0700

    All sql tasks

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,2 @@
+-- all records of the table second_table with score >= 10 of the database hbtn_0c_0 in your MySQL server.
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
