111fdcb0befe30f8c7223327e0efba3c087b5c39
Muawia24 <amuawia666@gmail.com>
2024-05-23 03:36:01 -0700

    13-count_shows_by_genre.sql

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -9,6 +9,6 @@ SELECT g.name AS 'genre', COUNT(*) as 'number_of_shows'
 FROM tv_genres g
 	INNER JOIN tv_show_genres t
 	ON g.id = t.genre_id
-GROUP BY g.name
+GROUP BY genre
 ORDER BY number_of_shows DESC;
 	
