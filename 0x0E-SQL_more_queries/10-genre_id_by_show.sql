36747f0d162d0f1c3bfa8adc08ed9fd64fe5924b
Muawia24 <amuawia666@gmail.com>
2024-05-23 03:19:19 -0700

    10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -3,7 +3,7 @@
 -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 -- You can use only one SELECT statement
 SELECT s.title, g.genre_id
-FROM tv_show_gennres g
-INNER JOIN tv_shows s
+FROM tv_shows s
+INNER JOIN tv_show_genres g
 ON s.id = g.show_id
 ORDER BY s.title, g.genre_id ASC;
