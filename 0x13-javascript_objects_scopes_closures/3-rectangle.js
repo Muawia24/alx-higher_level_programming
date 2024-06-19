a2864716487a294e5825c5ff3e3ba4bc00da7b80
Ahmed Muawia <amuawia666@gmail.com>
2024-06-19 21:53:24 +0000

    3-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (h > 0 && w > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let str1 = '';
+    let i;
+    let j;
+    for (i = 0; i < this.height; i++) {
+      for (j = 0; j < this.width; j++) {
+        str1 += 'x';
+      }
+      console.log(str1);
+      str1 = '';
+    }
+  }
+};
