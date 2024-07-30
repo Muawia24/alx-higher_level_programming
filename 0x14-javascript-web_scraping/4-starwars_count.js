58aedec3d2db9c569bfcb00edcff2edd9a034264
Ahmed Muawia <amuawia666@gmail.com>
2024-07-30 21:35:57 +0300

    4-starwars_count.js

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const request = require('request');
+const args = process.argv.slice(2);
+const url = args[0];
+const character = 'https://swapi-api.alx-tools.com/api/people/18/';
+
+let count = 0;
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const films = JSON.parse(body).results;
+
+    for (let result = 0; result < films.length; result++) {
+      if (films[result].characters.includes(character)) {
+        count += 1;
+      }
+    }
+    console.log(count);
+  }
+});
